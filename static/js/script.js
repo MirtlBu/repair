1ccdd589500883480991cb2e54f024b9388bf590
mirtlbu <bubuzoglo@gmail.com>
2017-12-09 17:29:24 +0300

    added tablet

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,15 @@
 $(function() {
-    console.log('hooray!');
+
+    var input = document.querySelector('.upload__file'),
+        textElem = document.querySelector('.upload__text'),
+        EMPTY = 'No file chosen';
+    input.addEventListener('change', function(e) {
+        if(this.files.length) {
+            textElem.innerHTML = e.target.value.split( '\\' ).pop();
+        }
+        else {
+            textElem.innerHTML = EMPTY;
+        }
+    });
+
 })

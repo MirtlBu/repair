9e39d2ef1b6748e2aad556ffdf41c0cf3945123f
mirtlbu <bubuzoglo@gmail.com>
2018-03-21 23:43:47 +0300

    finish point

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,23 @@
 $(function() {
+    resizeFotorama();
 
     var input = document.querySelector('.upload__file'),
         textElem = document.querySelector('.upload__text'),
         EMPTY = 'No file chosen';
+    textElem.innerHTML = EMPTY;
     input.addEventListener('change', function(e) {
         if(this.files.length) {
+            textElem.classList.remove('upload__text--empty');
             textElem.innerHTML = e.target.value.split( '\\' ).pop();
         }
         else {
+            textElem.classList.add('upload__text--empty');
+            textElem.innerHTML = EMPTY;
+        }
+    });
+    textElem.addEventListener('click', function(e) {
+        if(textElem.innerHTML) {
+            textElem.classList.add('upload__text--empty');
             textElem.innerHTML = EMPTY;
         }
     });
@@ -21,15 +31,26 @@ $(function() {
 
     $( "#accordion" ).accordion();
 
-    window.onresize = function(event) {
-        var $fotoramaDiv = $('.fotorama').fotorama();
-        var fotorama = $fotoramaDiv.data('fotorama');
-        console.log($('.fotorama').width());
-        // fotorama.resize({
-        //   width: $('.fotorama').width(),
-        //   height: $('.fotorama').height()
-        // });
+    function resizeFotorama() {
+        if (window.innerWidth < 1024) {
+            $('.fotorama').fotorama({
+              ratio: 800/850
+            });
+        }
+        else if (window.innerWidth >= 1024 && window.innerWidth < 1200) {
+            $('.fotorama').fotorama({
+              ratio: 800/570
+            });
+        }
+        else if (window.innerWidth >= 1200) {
+            $('.fotorama').fotorama({
+              ratio: 800/445
+            });
+        }
+    }
 
+    window.onresize = function(event) {
+        resizeFotorama();
     };
 
 })
